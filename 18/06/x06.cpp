0a4fde9dfe70b778132992c26616ccdbd1c87b03
Eiard <3067132996@qq.com>
2022-02-15 00:44:45 +0800

    2022/2/15

--- 18/06/x06.cpp
+++ 18/06/x06.cpp
@@ -5,5 +5,14 @@
 #include "x06.h"
 
 void x06Test(){
+    ElemType T[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    Sqlist Sq;
+    Sq.data = T;
+    Sq.length = sizeof(T) / sizeof(ElemType);
+
+    printf("The origin is ");
+    for (int i = 0; i < Sq.length; i++) {
+        printf("%d ", Sq.data[i]);
+    }
 
 }
