b40977d8e8fb2d2e9a5bd368b01492e923c44338
Eiard <3067132996@qq.com>
2022-02-19 22:02:55 +0800

    2022/2/15

--- 40/04/x04.cpp
+++ 40/04/x04.cpp
@@ -11,6 +11,18 @@ ElemType x40_04(LinkList &L){
 }
 
 void x40_04Test(){
+    LinkList L;
+    Init_Head(L);  // 初始化链表
 
+    ListInsertPrior_LNode(L, 1, 1);
+    ListInsertPrior_LNode(L, 1, 2);
+    ListInsertPrior_LNode(L, 1, 2);
+    ListInsertPrior_LNode(L, 1, 3);
+    printf(" The origin is ");
+    ListTraverse_LNode(L);
 
+    x40_04(L);
+
+    printf(" After change is ");
+    ListTraverse_LNode(L);
 }
