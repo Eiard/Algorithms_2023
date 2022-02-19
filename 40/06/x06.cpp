7c8183694a2ca34748e1d9462d501198cf0cf1b4
Eiard <3067132996@qq.com>
2022-02-19 23:04:26 +0800

    2022/2/15

--- 40/06/x06.cpp
+++ 40/06/x06.cpp
@@ -3,3 +3,29 @@
 //
 
 #include "x06.h"
+
+// 让元素递增有序
+ElemType x40_06(LinkList &L){
+
+
+    return OK;
+}
+
+void x40_06Test(){
+    LinkList L;
+    Init_Head(L);  // 初始化链表
+
+    ListInsertPrior_LNode(L, 1, 1);
+    ListInsertPrior_LNode(L, 1, 2);
+    ListInsertPrior_LNode(L, 1, 2);
+    ListInsertPrior_LNode(L, 1, 3);
+    ListInsertPrior_LNode(L, 1, 1);
+    ListInsertPrior_LNode(L, 1, 2);
+    printf(" The origin is ");
+    ListTraverse_LNode(L);
+
+    x40_06(L);
+
+    printf(" After change is ");
+    ListTraverse_LNode(L);
+}
--- 40/06/x06.h
+++ 40/06/x06.h
@@ -5,4 +5,10 @@
 #ifndef ALGORITHMS_X06_H
 #define ALGORITHMS_X06_H
 
+#include "../LinkList.h"
+
+ElemType x40_06(LinkList &L);
+
+void x40_06Test();
+
 #endif //ALGORITHMS_X06_H
