fa06e9d0078aff58931e7072d2dd4e2722f7b417
Eiard <3067132996@qq.com>
2022-02-21 17:06:35 +0800

    2022/2/21

--- 40/15/x15.cpp
+++ 40/15/x15.cpp
@@ -3,26 +3,31 @@
 //
 
 #include "x15.h"
-ElemType x40_15(LinkList &L){
+
+ElemType x40_15(LinkList &LA, LinkList &LB) {
+
 
     return OK;
 }
 
-void x40_15Test(){
-    LinkList L;
-    Init_Head(L);  // 初始化链表
+void x40_15Test() {
+    LinkList LA;
+    Init_Head(LA);  // 初始化链表
+    LinkList LB;
+    Init_Head(LB);  // 初始化链表
+    ListInsertPrior_LNode(LA, 1, 4);
+    ListInsertPrior_LNode(LA, 1, 3);
+    ListInsertPrior_LNode(LA, 1, 1);
+    ListInsertPrior_LNode(LB, 1, 6);
+    ListInsertPrior_LNode(LB, 1, 5);
+    ListInsertPrior_LNode(LB, 1, 2);
+    printf(" The origin LA is ");
+    ListTraverse_LNode(LA);
+    printf(" The origin LB is ");
+    ListTraverse_LNode(LB);
 
-    ListInsertPrior_LNode(L, 1, 1);
-    ListInsertPrior_LNode(L, 1, 2);
-    ListInsertPrior_LNode(L, 1, 2);
-    ListInsertPrior_LNode(L, 1, 3);
-    ListInsertPrior_LNode(L, 1, 1);
-    ListInsertPrior_LNode(L, 1, 2);
-    printf(" The origin is ");
-    ListTraverse_LNode(L);
+    x40_15(LA, LB);
 
-
-
-    printf(" After change is ");
-    ListTraverse_LNode(L);
+    printf(" After change LA is ");
+    ListTraverse_LNode(LA);
 }
