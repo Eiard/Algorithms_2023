fc4d15b090dd3085ee91865e14406995f24705de
Eiard <3067132996@qq.com>
2022-02-23 22:41:50 +0800

    2022/2/23

--- 40/25/x25.cpp
+++ 40/25/x25.cpp
@@ -4,14 +4,8 @@
 
 #include "x25.h"
 // 实现尽可能高效的算法 实现原先L a1 a2 a3 .. an  ---> a1,an,a2,an-1,a3,an-2....
-// a1 a2 a3 a4 a5 a6  2<->6
-// a1 a6 a3 a5 a4 a2  4<->5
-// a1 a6 a2 a4 a5 a3  6<->4
-// a1 a6 a2 a5 a4 a3
 
-//
-
-// a1 a6 a2 a5 a3 a4
+// 时间复杂度O(n) 空间复杂度O(1)
 ElemType x40_25(LinkList &L) {
     // 将链表后半段原地逆置
     LNode *p, *q, *r, *s;
@@ -24,28 +18,63 @@ ElemType x40_25(LinkList &L) {
     }
 
     // 此时p为链表中点
-    q = p->next;
-    p->next = nullptr;  // 后半段逆置
+    q = p->next;  //p为工作指针
+    p->next = nullptr;  // 后半段逆置  把p后面直接断开
+    // p后面直接为空
+    // 然后q q1 q2 q3 q4
+
     while (q) {
-        // p q q->next
-        // p p->next q r
-        //往后每一个结点都插入到p后面
-        r = q->next;  // 临时存储往后的结点
+        // p p1 p2
+        // q q1 q2
+
+        //将q1先记录在r中
+        r = q->next;  // 记录下一个结点的指针
+
+        // 采用对于p结点后的尾插法
+        // 让q->next 先指向p->next
+
+
+        // q->next 先记录p->next 避免p->next结点丢失
         q->next = p->next;
-        // p r
-        //
+        // p->next 就可以直接指向q
         p->next = q;
+        // 将存储的临时工作结点更新到q中
         q = r;
     }
 
+
+    // 从头开始
     s = L->next;
+    // 从中点开始
     q = p->next;
+
+    // a1 a2 a3 a4 a5 a6  逆置后
+    // a1 a2 a3 a6 a5 a4
+
+    // a6尾插法插入到a1后
+    // s: a1 a2 a3
+    // q: a6 a5 a4
+
+    // a5尾插法插入到a2后
+    // s: a1 a6 a2 a3
+    // q: a5 a4
+
+    // a4尾插法插入到a3后
+    // s: a1 a6 a2 a5 a3
+    // q: a4
+
+    // s: a1 a6 a2 a5 a3 a4
+
+
+
+
     p->next = nullptr;
-    while (q) {
-        r = q->next;
-        q->next = s->next;
-        s->next = q;
-        s = q->next;
+
+    while (q) {  // 还是尾插法
+        r = q->next;  // 临时保存下一个工作结点
+        q->next = s->next;  // 先保存s->next的值 存储到q->next  因为q->next 已经临时存储到r中
+        s->next = q;  // 将s->next 直接指向q结点 相当于插入
+        s = q->next;  //
         q = r;
     }
 
